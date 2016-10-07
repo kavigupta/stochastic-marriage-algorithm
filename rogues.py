0b70192be6b3ad27b3da70869d32d422fcd5f2a5
Kavi Gupta <kavi@globalmbox.com>
2016-10-06 20:46:05 -0700

    fixed rogues object to not create duplicates

--- rogues.py
+++ rogues.py
@@ -4,15 +4,19 @@ from random import randint
 class Rogues:
     def __init__(self, pairing, ranking):
         self.__rogues = []
+        used_ps = set()
         for p in pairing.people:
+            used_ps.add(p)
             q = pairing.partner_of(p)
-            print(p, q, list(ranking.all_preferred(p, q)))
             for q_star in ranking.all_preferred(p, q):
+                if q_star in used_ps:
+                    continue
                 p_star = pairing.partner_of(q_star)
-                print(p, q, q_star, p_star, ranking.prefers(q_star, p, p_star))
                 if ranking.prefers(q_star, p, p_star):
                     self.__rogues.append((p, q_star))
     def __len__(self):
         return len(self.__rogues)
     def select(self):
         return self.__rogues.pop(randint(0, len(self) - 1))
+    def __str__(self):
+        return "<rogues object with elements: %s>" % (tuple(self.__rogues),)
